cec78c93ec7e7e46d8020a5ddad261a5e202699e
Michał Tatkowski <michal.tatkowski6@gmail.com>
2025-08-26 20:22:00 +0200

    Integrate API service with user domain model and implement basic data fetching logic.

---
+++ gymapp_web/lib/features/auth/domain/user.dart
@@ -0,0 +1,22 @@
+﻿class User {
+  final String guid;
+  final String name;
+  final String email;
+  final String password;
+
+  User({
+    required this.guid,
+    required this.name,
+    required this.email,
+    required this.password,
+  });
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      guid: json['guid'],
+      name: json['name'],
+      email: json['email'],
+      password: json['password'],
+    );
+  }
+}
---
+++ gymapp_web/lib/services/api_service.dart
@@ -0,0 +1,34 @@
+﻿import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class ApiService<T> {
+  final String baseUrl = 'http://10.0.2.2:5035/api';
+
+  Future<T> get<T>(
+    String endpoint,
+    T Function(Map<String, dynamic>) fromJson,
+  ) async {
+    final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> jsonData = json.decode(response.body);
+      return fromJson(jsonData);
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+
+  Future<List<T>> getList<T>(
+    String endpoint,
+    T Function(Map<String, dynamic>) fromJson,
+  ) async {
+    final response = await http.get(Uri.parse('$baseUrl/$endpoint'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> jsonData = json.decode(response.body);
+      return jsonData.map((item) => fromJson(item)).toList();
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
